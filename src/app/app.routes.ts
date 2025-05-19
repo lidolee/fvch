9389a89d93377fc77c00576cec3afca952b4724e
Walid Ghazzo <195703027+lidolee@users.noreply.github.com>
2025-05-19 17:34:01 +0200

    base setup for component routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { OfferteComponent } from './offerte/offerte/offerte.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: OfferteComponent
+  }
+];
