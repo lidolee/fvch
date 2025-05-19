e03c2982862ad00852e60756e77fd84eaa3d9f30
Walid Ghazzo <195703027+lidolee@users.noreply.github.com>
2025-05-19 17:07:11 +0200

    update offerte.module.ts and component structure

--- src/app/offerte/offerte.module.ts
+++ src/app/offerte/offerte.module.ts
@@ -1,12 +1,38 @@
+/**
+ * @file offerte.module.ts
+ * @author lidolee
+ * @date 2025-05-19 14:52:51
+ * @description Offerte module for handling quote creation and management
+ */
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+import { OfferteComponent } from './offerte/offerte.component';
+import { KontaktdatenComponent } from './components/kontaktdaten/kontaktdaten.component';
+import { DienstleistungenComponent } from './components/dienstleistungen/dienstleistungen.component';
+import { ZusammenfassungComponent } from './components/zusammenfassung/zusammenfassung.component';
+import { KalkulationComponent } from './components/kalkulation/kalkulation.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgbModule,
+    // FV Components
+    OfferteComponent,
+    KontaktdatenComponent,
+    DienstleistungenComponent,
+    ZusammenfassungComponent,
+    KalkulationComponent
+  ],
+  exports: [
+    OfferteComponent
   ]
 })
 export class OfferteModule { }
