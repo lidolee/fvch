a9103ec5bdc4d3e91f53463e884be1b887a4c053
Walid Ghazzo <195703027+lidolee@users.noreply.github.com>
2025-05-27 20:18:45 +0200

    reset to new UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { OfferteComponent } from './offerte/offerte/offerte.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: OfferteComponent
   }
 ];
